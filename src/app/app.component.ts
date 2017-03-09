f91027452eb4ae0fb061035ac9993e9915328692
littl3hawk <gavin.bennette@gmail.com>
2017-03-09 17:05:22 +1100

    Checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,29 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-   selector: 'main-app',
-   template: '<h1>Hiya! First Component</h1>'
+    selector: 'main-app',
+    template: '<h1>Hiya! First Component</h1>'
 })
 
-export class AppComponent{
-    constructor() {}
+export class AppComponent {
+    name = 'Angular';
 
-
-/*import { Component, OnInit } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'selector',
-    templateUrl: 'feature.component.html'
-})
-
-export class AppComponent *implements OnInit* {
     constructor() { }
 
-    *ngOnInit() { }*
-}*/
+
+    /*import { Component, OnInit } from '@angular/core';
+    
+    @Component({
+        moduleId: module.id,
+        selector: 'selector',
+        templateUrl: 'feature.component.html'
+    })
+    
+    export class AppComponent *implements OnInit* {
+        constructor() { }
+    
+        *ngOnInit() { }*
+    }*/
 
 
 
